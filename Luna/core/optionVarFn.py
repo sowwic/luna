472d7919db872e33e9f9e394b99185245fad40e7
Dmitrii <loki.di90@gmail.com>
2020-07-30 11:50:16 +0100

    Add Luna optionVar enum and functioms

--- Luna/core/optionVarFn.py
+++ Luna/core/optionVarFn.py
@@ -1,4 +1,5 @@
 import logging
+import json
 import pymel.core as pm
 from Luna.utils.enumFn import Enum
 
@@ -6,5 +7,34 @@ LOGGER = logging.getLogger(__name__)
 
 
 class OptionVars(Enum):
-    prevWorkspace = "LunaPrevWorkspace"
-    locatorToolOptions = "LunaLocatorToolOptions"
+    """ Luna vars"""
+    previous_workspace = "LunaPrevWorkspace"
+    recent_workspaces = "LunaRecentWorkspaces"
+
+
+class ToolVars(Enum):
+    locator_tool = "LunaLocatorToolOptions"
+
+
+def getOptionVar(var, default="", asDict=False):
+    if var not in pm.optionVar and default:
+        # Convert to string if dictionary
+        if isinstance(default, dict):
+            default = json.dumps(default)
+        pm.optionVar.setdefault(var, default)
+
+    value = pm.optionVar[var]
+    if asDict:
+        value = json.loads(pm.optionVar[var])
+
+    return value
+
+
+def toggleOptionVar(var):
+    if var in pm.optionVar:
+        value = pm.optionVar[var]
+        if value == 0:
+            value = 1
+        elif value == 1:
+            value = 0
+        pm.optionVar[var] = value
